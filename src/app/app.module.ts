972f9aa71593653cbf830200145e0a09e683f9b1
Juan Pablo <jbernal@intelecto.co>
2023-06-05 17:17:41 -0500

    Se agregan importaciones necesarias en el proceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConteoComponent } from './components/conteo/conteo.component';
+import { RecaudoComponent } from './components/recaudo/recaudo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { ReporteComponent } from './components/reporte/reporte.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConteoComponent,
+    RecaudoComponent,
+    ReporteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatSortModule,
+    FormsModule,
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
