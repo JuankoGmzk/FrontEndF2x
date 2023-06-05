1591060b87be7d3a38222fd0558bd9709d036fd1
Juan Pablo <jbernal@intelecto.co>
2023-06-05 17:18:36 -0500

    Se crea el modelo de conteo, para posibles futuras mejoras

---
+++ src/app/models/conteo.ts
@@ -0,0 +1,9 @@
+export class Conteo{
+    id?:number;
+    estacion? : String;
+    sentido? : String;
+    hora? : String;
+    categoria? : String;
+    cantidad? : Number;
+    fecha? : String;
+}
